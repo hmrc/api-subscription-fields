f0ec04c629e3068ab9f2a8c4ec3819818f09e8e3
Pete Slater <pslater@equalexperts.com>
2017-11-22 15:11:23 +0000

    APIS-2888 - Tidy up of AppContext

--- app/uk/gov/hmrc/apisubscriptionfields/config/AppContext.scala
+++ app/uk/gov/hmrc/apisubscriptionfields/config/AppContext.scala
@@ -26,24 +26,13 @@ class AppContext @Inject()(val config: Config) extends ServicesConfig {
   lazy val isExternalTestEnvironment: Boolean = runModeConfiguration.getBoolean("isExternalTestEnvironment").getOrElse(false)
   lazy val devHubTitle: String = if (isExternalTestEnvironment) "Developer Sandbox" else "Developer Hub"
 
-  lazy val fetchApplicationTtlInSecs : Int = getConfig("fetchApplicationTtlInSeconds", runModeConfiguration.getInt)
-  lazy val fetchSubscriptionTtlInSecs : Int = getConfig("fetchSubscriptionTtlInSeconds", runModeConfiguration.getInt)
-
   lazy val publishApiDefinition = runModeConfiguration.getBoolean("publishApiDefinition").getOrElse(false)
   lazy val apiContext = runModeConfiguration.getString("api.context").getOrElse("api-subscription-fields")
-  lazy val access = runModeConfiguration.getConfig(s"api.access")
+  lazy val access = runModeConfiguration.getConfig("api.access")
 
   override def toString() = {
     "AppContext{" + (
-      Seq(s"fetchApplicationTtlInSecs=$fetchApplicationTtlInSecs",
-          s"fetchSubscriptionTtlInSecs=$fetchSubscriptionTtlInSecs",
-          s"isExternalTestEnvironment=$isExternalTestEnvironment"
+      Seq(s"isExternalTestEnvironment=$isExternalTestEnvironment"
       ) mkString ",") +"}"
   }
-
-  private def getConfig(key: String) = runModeConfiguration.getString(key)
-    .getOrElse(throw new RuntimeException(s"[$key] is not configured!"))
-
-  private def getConfig[T](key: String, f: String => Option[T]) = f(key)
-    .getOrElse(throw new RuntimeException(s"[$key] is not configured!"))
 }

40927789c603c74808937799f9bfff4d18eaa830
Luis Burgos <1550179+lburgos@users.noreply.github.com>
2019-12-30 13:10:42 +0000

    API-4089: remove unused code

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sbt clean coverage test it:test acceptance:test coverageReport
+sbt clean coverage test acceptance:test coverageReport
 python dependencyReport.py api-subscription-fields

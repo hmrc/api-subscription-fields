87059646cee3d1fa73c781d11b347570cb2797ae
Neil Frow <675806+worthydolt@users.noreply.github.com>
2022-09-07 12:03:46 +0100

    APID-520: update dependencies/plugins and fix slash syntax warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,15 @@ import play.core.PlayVersion
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
 
+  private lazy val mongoVersion = "0.70.0"
+  private lazy val bootstrapVersion = "7.2.0"
+
   private lazy val dependencies = Seq(
-    "uk.gov.hmrc"               %% "bootstrap-backend-play-28"        % "5.16.0",
-    "uk.gov.hmrc"               %% "simple-reactivemongo"             % "8.0.0-play-28",
+    "uk.gov.hmrc"               %% "bootstrap-backend-play-28"        % bootstrapVersion,
+    "uk.gov.hmrc"               %% "simple-reactivemongo"             % "8.1.0-play-28",
     "org.julienrf"              %% "play-json-derived-codecs"         % "6.0.0",
     "com.typesafe.play"         %% "play-json"                        % "2.9.2",
-    "uk.gov.hmrc"               %% "http-metrics"                     % "2.3.0-play-28",
+    "uk.gov.hmrc"               %% "http-metrics"                     % "2.5.0-play-28",
     "org.typelevel"             %% "cats-core"                        % "2.6.1",
     "eu.timepit"                %% "refined"                          % "0.9.13",
     "be.venneborg"              %% "play28-refined"                   % "0.6.0",
@@ -17,7 +20,7 @@ object AppDependencies {
   )
 
   private lazy val testDependencies = Seq(
-    "uk.gov.hmrc"               %% "reactivemongo-test"               % "5.0.0-play-28",
+    "uk.gov.hmrc"               %% "reactivemongo-test"               % "5.1.0-play-28",
     "org.scalatestplus.play"    %% "scalatestplus-play"               % "3.1.3",
     "org.mockito"               %% "mockito-scala-scalatest"          % "1.16.42",
     "org.pegdown"               %  "pegdown"                          % "1.6.0",

091f3165b7f2f746686fefd05f2ead21ac3927eb
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2025-03-13 16:00:21 +0000

    API-8284: Library upgrade relating to bobby warnings (#118)
    * API-8284: Library upgrade relating to boby warnings
    
    * API-8284: bump sbt-plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
 
-  private lazy val mongoVersion     = "2.1.0"
-  private lazy val bootstrapVersion = "9.5.0"
-  val commonDomainVersion           = "0.17.0"
+  private lazy val mongoVersion     = "2.6.0"
+  private lazy val bootstrapVersion = "9.11.0"
+  val commonDomainVersion           = "0.18.0"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
-    "uk.gov.hmrc"       %% "http-metrics"               % "2.8.0",
+    "uk.gov.hmrc"       %% "http-metrics"               % "2.9.0",
     "org.typelevel"     %% "cats-core"                  % "2.10.0",
     "eu.timepit"        %% "refined"                    % "0.10.2",
     "commons-validator"  % "commons-validator"          % "1.6",
